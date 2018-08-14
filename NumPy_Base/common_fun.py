1a24003a061a68b156088fc137bd483121ee3c8f
bullgo <brand_daijh@163.com>
2018-08-14 16:24:40 +0800

    常见金融统计量计算

--- NumPy_Base/common_fun.py
+++ NumPy_Base/common_fun.py
@@ -65,12 +65,12 @@ from datetime import datetime
 # 星期七 7 不开盘
 
 # def datestr2num(s):
-#     return datetime.strptime(s.decode('ascii'), "%Y-%m-%d").date().weekday()
+    # return datetime.strptime(s.decode('ascii'), "%Y-%m-%d").date().weekday()
 
 # dates, close= np.loadtxt('aapl.csv', delimiter=',', usecols=(0,4), unpack=True, converters={0:datestr2num})
-# # 测试
-# # raw ="1994-01-17"
-# # dates = datestr2num(raw)
+# 测试
+# raw ="1994-01-17"
+# dates = datestr2num(raw)
 
 # # 分组存储与比较
 # average = np.zeros(5)
@@ -88,3 +88,35 @@ from datetime import datetime
 # print("Highest average " + str(bottom))
 # print("Bottom day of the week ", + np.argmin(average))
 
+# 汇总数据
+## 周汇总
+def datestr2num(s):
+    return datetime.strptime(s.decode('ascii'), "%Y-%m-%d").date().weekday()
+
+dates, opens, high, low, close = np.loadtxt('aapl.csv', delimiter=',', usecols=(0,1,2,3,4), converters={0:datestr2num}, unpack=True)
+
+close = close[:16]
+dates = dates[:16]
+
+first_Monday = np.ravel(np.where(dates == 0))[0]
+# print("This is the 1st Monday: " + str(first_Monday))
+
+last_Friday = np.ravel(np.where(dates == 4))[-1]
+# print("This is the last Friday: " + str(last_Friday)) 
+
+week_indices = np.arange(first_Monday, last_Friday + 1)
+print("Weeks indices initial is " + str(week_indices))
+
+week_indices = np.split(week_indices, 3)
+print("Weeks indices after split " + str(week_indices))
+
+def summarize(a, o, h, l, c):
+    monday_open = o[a[0]]
+    week_high = np.max( np.take(h, a))
+    week_low = np.min(np.take(l,a))
+    friday_close = c[a[-1]]
+
+    return("APPL "+ str(monday_open) + str(week_high) + str(week_low) + str(friday_close))
+
+weeksummary = np.apply_along_axis(summarize, 1, week_indices, opens, high, low, close)
+print(str(weeksummary))
